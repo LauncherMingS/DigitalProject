67ae46bec3924c3734b5e143d9cb42cf3334b9bc
黃明聖 <s1091994@gm.pu.edu.tw>
2024-05-24 00:46:01 +0800

    ver 1

---
+++ Assets/Scripte/ScriptObject/CommanderData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Scriptable Object/CommanderData", fileName = "CommanderData", order = 6)]
+public class CommanderData : UnitData
+{
+    public float atkDamage;
+    public float atkFrequence;
+    public float atkRange;
+}
